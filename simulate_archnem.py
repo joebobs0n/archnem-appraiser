2b2c5c36de5387ca745372d4df74b9863a4cf3d1
Andy Monk <czech.monk90@gmail.com>
2022-05-15 18:05:56 -0700

    finalize scripts

--- simulate_archnem.py
+++ simulate_archnem.py
@@ -12,10 +12,11 @@ def getArgs():
 
     args.add_argument('-f', '--valuesfile', default='appraisal_data.json', type=Path,
                      help='File with all archnemesis modifier appraisals per rating. (Default=appraisal_data.json)')
-    args.add_argument('-m', '--n_mods', default=3, type=int,
-                     help='')
-    args.add_argument('-s', '--n_sims', default=1, type=int,
-                     help='')
+    mods = args.add_mutually_exclusive_group()
+    mods.add_argument('-m', '--n_mods', default=3, type=int,
+                     help='Number of random mods to simulate. (Default=3)')
+    mods.add_argument('-M', '--mods', default=None, nargs='+', type=str,
+                      help='Space separated list of mods to simulate. For a list of valid mods, check empty_data.json. (Default=None)')
 
     return args.parse_args()
 
@@ -26,37 +27,50 @@ def getMultiplier(types):
     return sum(mult)
 
 
-def simulate(data, n_mods):
-    mod_names = random.sample([k for k in data.keys() if k[0] != '_'], n_mods)
+def simulate(data, mod_names):
+    empty = {
+        'base rating': 0,
+        'synergy mult': 0,
+        'final rating': 0,
+        'tags': []
+    }
     monster = {
         'archnames': mod_names,
         'mods': [],
-        'AR': [0,[],[]],
-        'DR': [0,[],[]],
-        'BBR': [0,[],[]]
+        'AR': deepcopy(empty),
+        'DR': deepcopy(empty),
+        'BBR': deepcopy(empty)
     }
     for name in mod_names:
         monster['mods'].extend(data[name][0])
 
         for idx, rating in enumerate(['AR', 'DR', 'BBR'], start=1):
-            monster[rating][0] += data[name][idx][0]
+            monster[rating]['base rating'] += data[name][idx][0]
             types = [t for t in data[name][idx][1].split(',') if t != '']
-            monster[rating][2].extend(types)
+            monster[rating]['tags'].extend(types)
     for rating in ['AR', 'DR', 'BBR']:
-        monster[rating][1] = getMultiplier(monster[rating][2])
-        monster[rating][0] = monster[rating][0]*(data['_SYNERGY_MULT']**monster[rating][1])
+        monster[rating]['synergy mult'] = data['_SYNERGY_MULT']**getMultiplier(monster[rating]['tags'])
+        monster[rating]['tags'].sort()
+        monster[rating]['final rating'] = monster[rating]['base rating']*(monster[rating]['synergy mult'])
     print(json.dumps(monster, indent=2))
-
 
 def main():
     args = getArgs()
     with open('appraisal_data.json', 'r') as f:
         data = json.load(f)
 
-    archnem_monsters = []
-    for i in range(args.n_sims):
-        archnem_monsters.append(simulate(data, args.n_mods))
-
+    if args.mods == None:
+        mod_names = random.sample([k for k in data.keys() if k[0] != '_'], args.n_mods)
+    else:
+        with open('empty_data.json', 'r') as f:
+            valid_names = [n for n in json.load(f).keys() if n[0] != '_']
+        mod_names = []
+        for name in args.mods:
+            if name.lower() in valid_names:
+                mod_names.append(name.lower())
+            else:
+                print(f'{name} not found in valid archnem mod list')
+    simulate(data, mod_names)
 
 if __name__ == '__main__':
     main()
